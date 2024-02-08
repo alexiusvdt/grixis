e1b57390a216e83e2ea25a9af0046a253167ff43
alexiusvdt <alex.johnson293@gmail.com>
2024-02-08 12:58:55 -0800

    sketch out GET and POST

--- grixis/main.py
+++ grixis/main.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
-
+from methods import *
 
 if __name__ == "__main__":
    print("main!")
    load_dotenv()
    key = os.environ.get("API_KEY")
-   print(key)
+#    print(key)
