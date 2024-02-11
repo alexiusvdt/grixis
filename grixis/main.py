23954227fc19e989224fb41635963f48567b852e
alexiusvdt <alex.johnson293@gmail.com>
2024-02-11 11:04:36 -0800

    WIP: adding oauth verification

--- grixis/main.py
+++ grixis/main.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
 import requests
+from upload import upload
 
 def main():
     print("main!")
     #photos need metadata described in photo sphere xmp metadata to be published (https://developers.google.com/streetview/spherical-metadata)
     # metadata xmp example here: https://developers.google.com/streetview/spherical-metadata
-   
 
-
+   # grab the filepath
+   # error check files
+      # check that metadata is properly formatted
+      # check that all files are present
+      #
+   # call upload script
+   # cleanup & process next/close
+   upload(acc)
 
 if __name__ == "__main__":
    load_dotenv()
